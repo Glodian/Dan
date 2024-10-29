ba7a693a062d8a3389f2014a1349dbb7bcd03168
Glodian <glodiandantez7@gmail.com>
2024-10-29 16:39:29 +0300

    Add files via upload

---
+++ SimpleCalculator1.java
@@ -0,0 +1,12 @@
+public class SimpleCalculator1{
+public static void MultiplyNumbers(){
+int num1 = 4;
+int num2 = 8;
+int answer = num1 * num2;
+System.out.println(answer);
+}
+public static void main(String[]args){
+SimpleCalculator1 se = new SimpleCalculator1();
+se.MultiplyNumbers();
+}
+}
